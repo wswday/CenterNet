c74d051b6aebc766291274fd1cc57208fac0fa83
wswday <wswday@sina.com>
2019-05-06 17:45:57 +0800

    fix error when compiling at windows

--- src/lib/external/setup.py
+++ src/lib/external/setup.py
@@ -2,14 +2,24 @@ import numpy
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import platform
 
-extensions = [
-    Extension(
-        "nms", 
-        ["nms.pyx"],
-        extra_compile_args=["-Wno-cpp", "-Wno-unused-function"]
-    )
-]
+
+if platform.system() == 'Linux':
+    extensions = [
+        Extension(
+            "nms",
+            ["nms.pyx"],
+            extra_compile_args=["-Wno-cpp", "-Wno-unused-function"]
+        )
+    ]
+else:
+    extensions = [
+        Extension(
+            "nms",
+            ["nms.pyx"]
+        )
+    ]
 
 setup(
     name="coco",
